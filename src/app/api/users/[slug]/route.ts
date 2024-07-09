d45aa678d01be626b906b0380ba839828ead89b7
rahul0007-tech <rahultech730@gmail.com>
2024-07-09 19:14:27 +0545

    Started backend

---
+++ src/app/api/users/[slug]/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import pool from "@/app/libs/mysql";
+
+
+export async function GET(
+    request:  NextRequest,
+    { params }: { params: { slug: string } }
+) {
+    const slug = params.slug // user id
+    
+    try {
+        const db = await pool.getConnection()        
+        
+        const query = 'select * from account where id = ?'
+        const [rows] = await db.execute(query,[slug])
+        db.release()
+        
+        return NextResponse.json(rows)
+    } catch (error) {
+        return NextResponse.json({
+            error: error
+        }, { status: 500 })
+    }
+}
